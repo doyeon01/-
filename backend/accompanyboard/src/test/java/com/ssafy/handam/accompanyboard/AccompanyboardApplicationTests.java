59bb1f578692f41fd1a4a33a84403fbf814f4668
김민주 <mj1584.mk@gmail.com>
2024-09-11 17:58:38 +0900

    init: accompanyboard

---
+++ backend/accompanyboard/src/test/java/com/ssafy/handam/accompanyboard/AccompanyboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.handam.accompanyboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccompanyboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
