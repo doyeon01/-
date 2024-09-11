7b1bfa13508e29fade9043875f61a63799ade591
강동형 <buslaut@naver.com>
2024-09-11 17:47:38 +0900

    제작중

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import MainPage from './components/pages/MainPage';
+import { Survey } from './components/pages/Survey.tsx';
 
 const App: React.FC = () => {
   return (
     <>
-      <MainPage />
+      <Survey />
     </>
   );
 }
---
+++ frontend/src/components/pages/Survey.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Survey = () => {
+  return (
+    <div className="font-bold">가나다라마바사아</div>
+  )
+}
