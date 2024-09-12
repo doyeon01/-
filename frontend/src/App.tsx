66a4873241c252470490c9d517c5dd20a8b87333
강동형 <buslaut@naver.com>
2024-09-12 17:11:07 +0900

    Update frontend/src/App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
-<<<<<<< frontend/src/App.tsx
 import { SurveyPage } from './components/pages/SurveyPage.tsx';
 import MainPage from './components/pages/MainPage.tsx';
->>>>>>> frontend/src/App.tsx
 
 const App: React.FC = () => {
   return (
     <>
-<<<<<<< frontend/src/App.tsx
       <div className='w-full h-full bg-[#F4F4EE] font-TheJamsil'>
         <SurveyPage />
->>>>>>> frontend/src/App.tsx
       </div>
     </>
   );
