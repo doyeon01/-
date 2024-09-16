bf927c6738a6de99bc7e47e761595532dbfba1a8
고도연 <focus0007@naver.com>
2024-09-16 21:41:50 +0900

    design: 개인정보 컴포넌트 디자인 수정

---
+++ frontend/src/components/molecules/Card/PersonalDetail.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const PersonalDetail = () => {
+  return (
+    <div>PersonalDetail</div>
+  )
+}
--- frontend/src/components/pages/MyPage.tsx
+++ frontend/src/components/pages/MyPage.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { PersonalInfo } from '../organisms/PersonalInfo';
+import { PersonalInfo } from '../molecules/Card/PersonalInfo';
 
 export const MyPage = () => {
   return (
-    <div className="container mx-auto p-5 max-w-4xl bg-white rounded-lg mt-40 mb-20">
-      <PersonalInfo />
-      <hr className="border-t-10 pb-5 " />
-
-    </div>
+    <>
+    <PersonalInfo />
+    </>
+    
   );
 }
