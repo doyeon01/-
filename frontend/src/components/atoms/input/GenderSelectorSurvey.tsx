a89ab04c59026f101d4c7d48a638c598577f630c
강동형 <buslaut@naver.com>
2024-09-20 15:00:34 +0900

    복구 완료

--- frontend/src/components/atoms/input/GenderSelectorSurvey.tsx
+++ frontend/src/components/atoms/input/GenderSelectorSurvey.tsx
@@ -8,11 +8,11 @@ interface Props {
 export const GenderSelector: React.FC<Props> =({Gender, OnGenderChange})=>{
     return(
         <div className='flex justify-center items-center gap-[10px]'>
-            <label htmlFor='gender1' className={`w-[175px] h-[88px] rounded-[20px] bg-blue flex flex-row justify-center items-center cursor-pointer ${Gender === 'MALE' ? 'bg-blue-500 text-white' : 'bg-white text-black'}`}>
+            <label htmlFor='gender1' className={`w-[120px] h-[50px] rounded-[10px] bg-blue flex flex-row justify-center items-center cursor-pointer ${Gender === 'MALE' ? 'bg-blue-500 text-white' : 'bg-white text-black'}`}>
             <input type='radio' name='gender' id='gender1' className='hidden' value='MALE' onChange={OnGenderChange}/>
             <span>남자</span>
             </label>
-            <label htmlFor='gender2' className={`w-[175px] h-[88px] rounded-[20px] bg-blue flex flex-row justify-center items-center cursor-pointer ${Gender === 'FEMALE' ? 'bg-blue-500 text-white' : 'bg-white text-black'}`}>
+            <label htmlFor='gender2' className={`w-[120px] h-[50px] rounded-[10px] bg-blue flex flex-row justify-center items-center cursor-pointer ${Gender === 'FEMALE' ? 'bg-blue-500 text-white' : 'bg-white text-black'}`}>
             <input type='radio' name='gender' id='gender2' className='hidden' value='FEMALE' onChange={OnGenderChange}/>
             <span>여자</span>
             </label>
