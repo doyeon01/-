f4c8afa46447fa1e9bb9d1c2ac9d8de3994a881e
고도연 <focus0007@naver.com>
2024-09-24 13:30:25 +0900

    design: 포토카드 다운로드 기능 구현

--- frontend/src/components/molecules/Card/PhtotCard.tsx
+++ frontend/src/components/molecules/Card/PhtotCard.tsx
@@ -1,18 +1,20 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { ModalPhotoCard } from '../../organisms/Modal/ModalPhotoCard';
+import { DownLoadIcon } from '../../../assets/icons/svg';
 
 interface PhotoCardProps {
   title: string;
   address?: string; 
   testimg: string;
-  showButton?: boolean; // 버튼을 보여줄지 여부를 결정하는 속성
+  showButton?: boolean; // 여행 일정 보기, 포토카드 생성 버튼 보여줄지 여부를 결정하는 속성
+  showDownLoadButton?: boolean;
 }
 
-export const PhotoCard: React.FC<PhotoCardProps> = ({ title, address, testimg, showButton = false }) => {
+export const PhotoCard: React.FC<PhotoCardProps> = ({ title, address, testimg, showButton, showDownLoadButton = false }) => {
   const [showModal, setShowModal] = useState(false); // 모달 상태 관리
+  const nav = useNavigate();
 
-  const nav = useNavigate()
   // 모달 열기 함수
   const handleOpenModal = () => {
     setShowModal(true);
@@ -23,7 +25,16 @@ export const PhotoCard: React.FC<PhotoCardProps> = ({ title, address, testimg, s
     setShowModal(false);
   };
 
-  
+  // 이미지 다운로드 함수
+  const handleDownloadImage = () => {
+    // a 태그를 이용한 다운로드 구현
+    const link = document.createElement('a');
+    link.href = testimg;  // 다운로드할 이미지 URL
+    link.download = title; // 파일 이름
+    document.body.appendChild(link); // 링크를 body에 추가
+    link.click(); // 링크 클릭으로 다운로드 실행
+    document.body.removeChild(link); // 링크 제거
+  };
 
   return (
     <div>
@@ -52,15 +63,25 @@ export const PhotoCard: React.FC<PhotoCardProps> = ({ title, address, testimg, s
           </div>
         )}
 
+        {/* 다운로드 버튼 */}
+        {showDownLoadButton && (
+          <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-0 transition-all duration-300 group-hover:bg-opacity-50">
+            <button 
+              className="text-black opacity-0 bg-opacity-80 hover:bg-opacity-100 py-2 px-3 bg-white rounded-lg transition-opacity duration-300 group-hover:opacity-100"
+              onClick={handleDownloadImage}
+            >
+              <DownLoadIcon />
+            </button>
+          </div>
+        )}
+
         <div className="absolute bottom-0 left-0 w-full p-4 bg-gradient-to-t from-black to-transparent text-white">
           <h3 className="text-lg font-bold">{title}</h3>
           {address && <p className="text-sm">{address}</p>}
         </div>
-
-       
       </div>
 
-        {showModal && <ModalPhotoCard onClose={handleCloseModal} />}
+      {showModal && <ModalPhotoCard onClose={handleCloseModal} />}
     </div>
   );
 };
