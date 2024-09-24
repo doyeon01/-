ee7942e683e660385f23b160a12339bdb70f6c54
김민주 <mj1584.mk@gmail.com>
2024-09-24 13:40:39 +0900

    docs: vite.config.ts 수정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,5 +6,14 @@ export default defineConfig({
     define: {
         'process.env': process.env 
     },
-    base: './'
+    base: './',
+    build: {
+        rollupOptions: {
+            output: {
+                entryFileNames: 'assets/[name].js',
+                chunkFileNames: 'assets/[name].js',
+                assetFileNames: 'assets/[name].[ext]'
+            }
+        }
+    }
 });
