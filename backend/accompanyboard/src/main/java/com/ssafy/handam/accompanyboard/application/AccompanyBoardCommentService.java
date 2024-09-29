bd6b9e14adc90d3690ea9299841699cfb986d314
김민주 <mj1584.mk@gmail.com>
2024-09-30 01:25:15 +0900

    feat: 동행게시글 댓글 조회 구현 완료

--- backend/accompanyboard/src/main/java/com/ssafy/handam/accompanyboard/application/AccompanyBoardCommentService.java
+++ backend/accompanyboard/src/main/java/com/ssafy/handam/accompanyboard/application/AccompanyBoardCommentService.java
@@ -5,7 +5,9 @@ import com.ssafy.handam.accompanyboard.domain.entity.Comment;
 import com.ssafy.handam.accompanyboard.domain.service.AccompanyBoardCommentDomainService;
 import com.ssafy.handam.accompanyboard.presentation.request.comment.AccompanyBoardCommentCreationRequest;
 import com.ssafy.handam.accompanyboard.presentation.response.comment.AccompanyBoardCommentResponse;
+import com.ssafy.handam.accompanyboard.presentation.response.comment.AccompanyBoardCommentsResponse;
 import jakarta.transaction.Transactional;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -19,5 +21,17 @@ public class AccompanyBoardCommentService {
     public AccompanyBoardCommentResponse createComment(AccompanyBoardCommentCreationRequest request) {
         Comment comment = accompanyBoardCommentDomainService.createComment(request);
         return AccompanyBoardCommentResponse.of(AccompanyBoardCommentDto.of(comment));
+    }
+
+    public AccompanyBoardCommentsResponse getCommentsByAccompanyBoardArticleId(Long accompanyBoardArticleId) {
+        List<AccompanyBoardCommentDto> comments = getAccompanyBoardCommentsDtoList(
+                accompanyBoardCommentDomainService.getCommentsByAccompanyBoardArticleId(accompanyBoardArticleId));
+        return AccompanyBoardCommentsResponse.of(comments);
+    }
+
+    public List<AccompanyBoardCommentDto> getAccompanyBoardCommentsDtoList(List<Comment> comments) {
+        return comments.stream()
+                .map(AccompanyBoardCommentDto::of)
+                .toList();
     }
 }
