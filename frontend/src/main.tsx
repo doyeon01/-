0718f7cfa47042b391751e10ca194f284c69e021
고도연 <focus0007@naver.com>
2024-10-01 16:20:56 +0900

    feat: 로그인 기능 구현 중

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,17 @@
 // import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RecoilRoot } from 'recoil';
+import { CookiesProvider } from 'react-cookie'
 import App from './App';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   // <StrictMode>
+  <CookiesProvider>
     <RecoilRoot>
       <App />
     </RecoilRoot>
+  </CookiesProvider>
+
   // </StrictMode>
 );
