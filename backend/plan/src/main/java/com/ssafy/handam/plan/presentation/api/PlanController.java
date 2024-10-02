1a125dfd704242330b1026f59817bbf653cddf1f
sangmin0806 <cjstkd2866@naver.com>
2024-10-02 10:29:01 +0900

    fix: 빌드 오류 수정

--- backend/plan/src/main/java/com/ssafy/handam/plan/presentation/api/PlanController.java
+++ backend/plan/src/main/java/com/ssafy/handam/plan/presentation/api/PlanController.java
@@ -17,7 +17,6 @@ import static com.ssafy.handam.plan.application.common.ApiUtils.success;
 public class PlanController {
     @PostMapping("/")
     public ApiResult<Void> getPlan(HttpServletRequest request,@RequestBody List<PlanServiceRequest> planServiceRequests) {
-
         return success(null);
     }
 }
