944a2413dae9c003a5865ac19224615803286169
강동형 <buslaut@naver.com>
2024-10-04 11:08:27 +0900

    m

--- frontend/src/services/api/RegisterUser.ts
+++ frontend/src/services/api/RegisterUser.ts
@@ -3,7 +3,7 @@ import { RegisterUserType } from '../../model/RegisterUserType';
 
 export const RegisterUser = async (id:number)=>{
     try {
-        const response = await axios.post<RegisterUserType>(`http://j11c205.p.ssafy.io/api/v1/user/${id}/survey`);
+        const response = await axios.post<RegisterUserType>(`https://j11c205.p.ssafy.io/api/v1/user/${id}/survey`);
         return response.data;
       } catch (error) {
         throw new Error('API 요청 중 오류가 발생했습니다.');
@@ -12,7 +12,7 @@ export const RegisterUser = async (id:number)=>{
 //RegisterUser.ts
 export const GetFeedFood = async(keyword:String,page:number,size:number)=>{
   try{
-    const response = await axios.get(`http://j11c205.p.ssafy.io/api/v1/feeds/search?keyword=${keyword}&page=${page}&size=${size}`)
+    const response = await axios.get(`https://j11c205.p.ssafy.io/api/v1/feeds/search?keyword=${keyword}&page=${page}&size=${size}`)
     return response.data
   } catch(error){
     throw new Error('API error')
