a06395673aa60164dda98f9d2ddab9e1bb81f8c3
sangmin0806 <cjstkd2866@naver.com>
2024-10-05 22:53:15 +0900

    fix: scg 쿠키 로그

--- backend/SCG/src/main/java/com/ssafy/handam/scg/filters/LoggingGlobalFilter.java
+++ backend/SCG/src/main/java/com/ssafy/handam/scg/filters/LoggingGlobalFilter.java
@@ -3,6 +3,7 @@ package com.ssafy.handam.scg.filters;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpCookie;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
@@ -16,6 +17,11 @@ public class LoggingGlobalFilter implements GlobalFilter {
     public Mono<Void> filter(ServerWebExchange exchange, org.springframework.cloud.gateway.filter.GatewayFilterChain chain) {
         String requestUri = exchange.getRequest().getURI().toString();
         log.info("Routing request to URI: {}", requestUri);
+        exchange.getRequest().getCookies().forEach((name, cookies) -> {
+            for (HttpCookie cookie : cookies) {
+                log.info("Cookie Name: {}, Cookie Value: {}", cookie.getName(), cookie.getValue());
+            }
+        });
         return chain.filter(exchange);
     }
 }
