ec73ddd8d84489e1a8ba8201d217143c397d581c
고도연 <focus0007@naver.com>
2024-10-07 01:03:36 +0900

    fix: 배포 오류 수정

--- frontend/src/components/pages/MyPage.tsx
+++ frontend/src/components/pages/MyPage.tsx
@@ -9,7 +9,6 @@ import { UserIcon } from '../../assets/icons/svg'; // UserIcon import
 
 export const MyPage = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  // const [reloadFeed, setReloadFeed] = useState(false); // 피드 리로드 트리거 상태 추가
   const nav = useNavigate();
   const [image, setImage] = useState<string | null>(null); // 초기값을 null로 변경
   const [nickName, setNickName] = useState<string | null>(''); // string | null 허용
@@ -27,7 +26,6 @@ export const MyPage = () => {
   // 글 작성 완료 후 탭을 1번으로 설정하고 모달을 닫음 + 피드 리로드
   const handleCompleteModal = () => {
     setIsModalOpen(false);
-    // setReloadFeed(prev => !prev); // 피드를 리로드하도록 상태 변경
     nav('/my', { state: { activeTab: 'tab1' } });
   };
 
@@ -81,7 +79,7 @@ export const MyPage = () => {
         </div>
       </div>
       <hr />
-      <PersonalDetailTab reloadFeed={reloadFeed} /> {/* reloadFeed prop 추가 */}
+      <PersonalDetailTab /> 
     </div>
   );
 };
