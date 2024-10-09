db877d8f859a770044c53c94a6302caf78284178
고도연 <focus0007@naver.com>
2024-10-09 12:40:55 +0900

    design: survey 페이지 디자인 수정

--- frontend/src/components/atoms/button/ButtonNext.tsx
+++ frontend/src/components/atoms/button/ButtonNext.tsx
@@ -8,11 +8,14 @@ export interface Props {
 
 export const ButtonNext:React.FC<Props>=({text, onClick})=>{
     return(
-        <button className='w-[50px] h-[23px] rounded-[6px] bg-[#707C60] flex justify-center items-center gap-[2px]' onClick={onClick}>
-            <div className='text-[12px] text-white'>
+        <button className='w-[50px] h-[23px] rounded-[6px] bg-white flex justify-center items-center gap-[2px]' onClick={onClick}>
+            <div className='text-[12px] text-black'>
                 {text}
             </div>
-            <img src={Vector} alt="벡터" className='w-[7px] h-[11px]' />
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-3">
+            <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+            </svg>
+
         </button>
     )
 }
