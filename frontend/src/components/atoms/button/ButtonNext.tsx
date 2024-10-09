2a5813477fdae96837069d453b0c9b4b1069708a
고도연 <focus0007@naver.com>
2024-10-09 13:49:25 +0900

    fix: 배포오류수정

--- frontend/src/components/atoms/button/ButtonNext.tsx
+++ frontend/src/components/atoms/button/ButtonNext.tsx
@@ -1,21 +1,31 @@
-import React from 'react'
-import Vector from '../../../assets/statics/Vector.png'
+import React from 'react';
 
 export interface Props {
     text: string; 
     onClick?: () => void;
 }
 
-export const ButtonNext:React.FC<Props>=({text, onClick})=>{
-    return(
-        <button className='w-[50px] h-[23px] rounded-[6px] bg-white flex justify-center items-center gap-[2px]' onClick={onClick}>
-            <div className='text-[12px] text-black'>
-                {text}
-            </div>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-3">
-            <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+export const ButtonNext: React.FC<Props> = ({ text, onClick }) => {
+    return (
+        <button
+            className="w-[50px] h-[23px] rounded-[6px] bg-white flex justify-center items-center gap-[2px]"
+            onClick={onClick}
+        >
+            <div className="text-[12px] text-black">{text}</div>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="size-3"
+            >
+                <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m8.25 4.5 7.5 7.5-7.5 7.5"
+                />
             </svg>
-
         </button>
-    )
-}
+    );
+};
