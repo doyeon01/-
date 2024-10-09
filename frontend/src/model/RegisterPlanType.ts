9a05d4e2afb8d4131e384b7487879a23053f9a16
강동형 <buslaut@naver.com>
2024-10-09 17:50:55 +0900

    m

---
+++ frontend/src/model/RegisterPlanType.ts
@@ -0,0 +1,39 @@
+export interface TravelPlan {
+    title: string;
+    startDate: string;
+    endDate: string;
+    dayPlans: DayPlan[];
+  }
+  
+interface DayPlan {
+day: number;
+plans: Plan[];
+}
+  
+type Plan = FeedPlan | PlacePlan;
+  
+interface FeedPlan {
+    type: "feed";
+    userId: number;
+    placeName: string;
+    title: string;
+    content: string;
+    imageUrl: string;
+    address1: string;
+    address2: string;
+    longitude: number;
+    latitude: number;
+    placeType: string;
+    details: string;
+  }
+  
+interface PlacePlan {
+    type: "place";
+    placeName: string;
+    roadAddressName: string;
+    addressName: string;
+    placeType: string;
+    x: number;
+    y: number;
+    details: string;
+  }
