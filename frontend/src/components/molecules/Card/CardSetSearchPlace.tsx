c843930576663c4047ec037ec0111ff723827bb1
kochungwon <yud01063@gmail.com>
2024-10-10 11:14:26 +0900

    오류수정

--- frontend/src/components/molecules/Card/CardSetSearchPlace.tsx
+++ frontend/src/components/molecules/Card/CardSetSearchPlace.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FeedType } from '../../../model/SearchingFeedType';
 import { getFeed } from '../../../services/api/RegisterUser';
 import { UserIconMini } from '../../../assets/icons/svg';
