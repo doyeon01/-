13c7fd7089b925a0fb8cc66c663d09b28365766d
고도연 <focus0007@naver.com>
2024-10-10 09:41:44 +0900

    fix: 디자인 수정

--- frontend/src/components/organisms/Modal/ModalSelectFeed.tsx
+++ frontend/src/components/organisms/Modal/ModalSelectFeed.tsx
@@ -121,7 +121,7 @@ export const ModalSelectFeed: React.FC<ModalSelectFeedProps> = ({ totalPlanId, o
             <div
             className="col-span-3 flex justify-center items-center h-80 mt-14 bg-[#e5e2d9] rounded-lg"
             onClick={onClose}>
-             <p className="text-[#b6afa9] text-xl pr-2 ">여행일정이 없습니다</p> 
+             <p className="text-[#b6afa9] text-xl pr-2 ">게시물이 존재하지 않습니다.</p> 
              <OutIcon/>
            </div>          
           )}
